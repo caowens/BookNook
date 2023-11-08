73718141df9e613ac5a19397881481839745b439
caowens <caowens555@yahoo.com>
2023-11-07 20:10:15 -0600

    Implement basic table view

--- BookNook/ViewController.swift
+++ BookNook/ViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var homeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        homeTableView.dataSource = self
     }
 
 
